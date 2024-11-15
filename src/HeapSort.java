0de9d8ba4edea6867a8fe796cadc941f5ab77885
eltammie <lennminh@gmail.com>
2024-11-15 15:40:39 -0500

    q4

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -2,8 +2,38 @@ import java.util.Arrays;
 
 public class HeapSort {
     public static void heapSort(Integer[] values) {
-        // Intentionally not implemented -- see homework assignmnet
-        throw new UnsupportedOperationException("Not yet implemented.");
+        int size = values.length;
+
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(values, size, i);
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            int temp = values[0];
+            values[0] = values[i];
+            values[i] = temp;
+
+            heapify(values, i, 0);
+        }
+    }
+
+    public static void heapify(Integer[] values, int size, int rootIndex) {
+        int leftChildIndex = 2 * rootIndex + 1;
+        int rightChildIndex = 2 * rootIndex + 2;
+        int indexOfSmallest = rootIndex;
+
+        if (leftChildIndex < size && values[leftChildIndex] < values[indexOfSmallest]) {
+            indexOfSmallest = leftChildIndex;
+        }
+        if (rightChildIndex < size && values[rightChildIndex] < values[indexOfSmallest]) {
+            indexOfSmallest = rightChildIndex;
+        }
+        if (indexOfSmallest != rootIndex) {
+            int temp = values[rootIndex];
+            values[rootIndex] = values[indexOfSmallest];
+            values[indexOfSmallest] = temp;
+
+            heapify(values, size, indexOfSmallest);
+        }
     }
 
     public static void main(String[] args) {
